3e423a19064b32dfa310e941b8b7f2946ebd0f1b
Ashnage Narnesfugen <50156611+AshnageNarnesfugen@users.noreply.github.com>
2022-09-17 23:03:50 -0500

    ddddd

--- js/main.js
+++ js/main.js
@@ -105,10 +105,13 @@ jQuery(() => {
     var observer = new IntersectionObserver(function(entries) {
         if (entries[0].isIntersecting === true) {
             var titles = $('.content-text mark')
-            $(titles).shuffleLetters({
-                "step": 30,
-                "fps": 60,
-                "text": $(this).attr('data-text')
+
+            $(titles).each(() => {
+                $(this).shuffleLetters({
+                    "step": 30,
+                    "fps": 60,
+                    "text": $(this).attr('data-text')
+                })
             })
         }
     }, { threshold: [1] });
