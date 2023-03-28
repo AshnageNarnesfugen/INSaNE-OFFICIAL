4e68ddd4d0f82595be3dc8e46871b104f0bc84c0
Ashnage Narnesfugen <50156611+AshnageNarnesfugen@users.noreply.github.com>
2023-03-28 17:55:00 -0600

    asdasdasd

--- js/main.js
+++ js/main.js
@@ -31,80 +31,81 @@ jQuery(() => {
                 });
             }
             */
-            function checkCookieConsent() {
-                var cookieConsent = Cookies.get('cookie-consent');
-                if (cookieConsent) {
-                  // Cookie has been set, continue with normal cookie logic
-                  handleCookies();
-                } else {
-                  // Show cookie consent banner
-                  $('#cookie-consent').show();
-              
-                  // Handle user's consent choice
-                  $('#accept-cookies').click(function() {
+            var cookieConsent = Cookies.get('cookie-consent');
+            var cookieRejected = Cookies.get('cookie-rejected');
+
+            if (cookieConsent || cookieRejected) {
+                // User has given cookie consent or rejected cookies, continue with normal cookie logic
+                handleCookies();
+            } else {
+                // Show cookie consent banner
+                $('#cookie-consent').show();
+
+                // Handle user's consent choice
+                $('#accept-cookies').click(function() {
                     // Set cookie consent
                     Cookies.set('cookie-consent', 'true', {
-                      expires: 365
+                        expires: 365
                     });
-              
+
                     // Hide cookie consent banner
                     $('#cookie-consent').hide();
-              
+
                     // Continue with normal cookie logic
                     handleCookies();
-                  });
-              
-                  $('#reject-cookies').click(function() {
+                });
+
+                $('#reject-cookies').click(function() {
+                    // Set cookie rejection
+                    Cookies.set('cookie-rejected', 'true', {
+                        expires: 365
+                    });
+
                     // Hide cookie consent banner
                     $('#cookie-consent').hide();
-              
+
                     // Continue with normal cookie logic without setting any cookies
                     handleCookies(false);
-                  });
-                }
-              }
-              
-              function handleCookies(allowCookies = true) {
+                });
+            }
+
+            function handleCookies(allowCookies = true) {
                 if (allowCookies) {
-                  // Cookies are allowed, continue with normal cookie logic
-                  var language = Cookies.get('language');
-                  if (language) {
-                    // Redirect user to the appropriate language version of the page
-                    if (language === 'es' && window.location.pathname !== '/es') {
-                      window.location.href = 'https://insane-bh.space/es';
-                    } else if (language === 'en' && window.location.pathname !== '/') {
-                      window.location.href = 'https://insane-bh.space';
+                    // Cookies are allowed, continue with normal cookie logic
+                    var language = Cookies.get('language');
+                    if (language) {
+                        // Redirect user to the appropriate language version of the page
+                        if (language === 'es' && window.location.pathname !== '/es') {
+                            window.location.href = 'https://insane-bh.space/es';
+                        } else if (language === 'en' && window.location.pathname !== '/') {
+                            window.location.href = 'https://insane-bh.space';
+                        }
+                    } else {
+                        // Get user's language from browser preferences
+                        var userLang = navigator.language || navigator.userLanguage;
+
+                        // Get user's location using IP geolocation
+                        $.getJSON('https://ipapi.co/json/', function(data) {
+                            var userCountry = data.country_code;
+
+                            // Check if user's language is not English and country is not the US or Canada
+                            if (userLang != 'en' && userCountry != 'US' && userCountry != 'CA') {
+                                // Redirect user to Spanish version of the page
+                                Cookies.set('language', 'es');
+                                window.location.href = 'https://insane-bh.space/es';
+                            } else {
+                                // Redirect user to English version of the page
+                                Cookies.set('language', 'en');
+                                window.location.href = 'https://insane-bh.space';
+                            }
+                        });
                     }
-                  } else {
-                    // Get user's language from browser preferences
-                    var userLang = navigator.language || navigator.userLanguage;
-              
-                    // Get user's location using IP geolocation
-                    $.getJSON('https://ipapi.co/json/', function(data) {
-                      var userCountry = data.country_code;
-              
-                      // Check if user's language is not English and country is not the US or Canada
-                      if (userLang != 'en' && userCountry != 'US' && userCountry != 'CA') {
-                        // Redirect user to Spanish version of the page
-                        Cookies.set('language', 'es');
-                        window.location.href = 'https://insane-bh.space/es';
-                      } else {
-                        // Redirect user to English version of the page
-                        Cookies.set('language', 'en');
-                        window.location.href = 'https://insane-bh.space';
-                      }
-                    });
-                  }
                 } else {
-                  // Cookies are not allowed, disable cookie functionality
-                  Cookies.defaults.expires = -1; // Set all cookies to expire immediately
-                  // ...
+                    // Cookies are not allowed, disable cookie functionality
+                    Cookies.defaults.expires = -1; // Set all cookies to expire immediately
+                    // ...
                 }
-              }
-              
-              $(document).ready(function() {
-                checkCookieConsent();
-              }); 
+            }
 
     $.fn.clickToggle = function(func1, func2) {
         var funcs = [func1, func2];
