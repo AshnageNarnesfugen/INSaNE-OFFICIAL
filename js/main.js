cd82c080964d42869747dcea62340f24ad999042
Ashnage Narnesfugen <50156611+AshnageNarnesfugen@users.noreply.github.com>
2023-06-28 16:03:02 -0600

    i'm not okay

--- js/main.js
+++ js/main.js
@@ -437,7 +437,7 @@ jQuery(() => {
 
 	function positionSecondDiv() {
 		var topPosition = $('.mark').outerHeight() + 20;
-		$('#letter').css('top', `-${topPosition}`);
+		$('#letter').css('top', `-${topPosition}px`);
 	  }
 	
 	  positionSecondDiv(); // Initial positioning
