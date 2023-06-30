7fa096d0faac3bd99a060d088c5452d83a1e52e0
Ashnage Narnesfugen <50156611+AshnageNarnesfugen@users.noreply.github.com>
2023-06-30 08:17:06 -0600

    Lazy load test

--- js/main.js
+++ js/main.js
@@ -1,4 +1,63 @@
 jQuery(() => {
+	class LazyCSSLoader {
+		constructor() {
+		  this.options = {
+			root: null, // Use the viewport as the root
+			rootMargin: '0px', // No margin
+			threshold: 0.1 // Trigger when 10% of the element is visible
+		  };
+	  
+		  this.observer = new IntersectionObserver(this.handleIntersection.bind(this), this.options);
+		}
+	  
+		loadCSS(cssURL) {
+		  // Check if the CSS file is already loaded in the document
+		  if ($(`link[href="${cssURL}"]`).length > 0) {
+			return Promise.resolve();
+		  }
+	  
+		  // Create a link element and set its attributes
+		  const link = document.createElement('link');
+		  link.rel = 'stylesheet';
+		  link.href = cssURL;
+	  
+		  // Add the link element to the document's head
+		  document.head.appendChild(link);
+	  
+		  // Return a promise that resolves when the CSS file is loaded
+		  return new Promise((resolve, reject) => {
+			link.onload = () => resolve();
+			link.onerror = () => reject(new Error(`Failed to load CSS: ${cssURL}`));
+		  });
+		}
+	  
+		handleIntersection(entries, observer) {
+		  entries.forEach(entry => {
+			if (entry.isIntersecting) {
+			  const cssURL = entry.target.getAttribute('data-href');
+			  this.loadCSS(cssURL)
+				.then(() => {
+				  observer.unobserve(entry.target);
+				})
+				.catch(error => {
+				  console.error(error);
+				});
+			}
+		  });
+		}
+	  
+		observeCSS() {
+		  const lazyCSSElements = $('[data-href]');
+		  lazyCSSElements.each((index, element) => {
+			this.observer.observe(element);
+		  });
+		}
+	  }
+	  
+	  const lazyCSSLoader = new LazyCSSLoader();
+	  lazyCSSLoader.observeCSS();
+	  
+
 	class LazyVideoLoader {
 		constructor() {
 		  this.options = {
@@ -71,109 +130,6 @@ jQuery(() => {
 	  // Usage:
 	  const lazyVideoLoader = new LazyVideoLoader();
 	  lazyVideoLoader.loadVideos();
-	  
-	  /*
-	 	class LazyImageLoader {
-		constructor() {
-		  this.options = {
-			root: null, // Use the viewport as the root
-			rootMargin: '0px', // No margin
-			threshold: 0.1 // Trigger when 10% of the image is visible
-		  };
-	  
-		  this.observer = new IntersectionObserver(this.handleIntersection.bind(this), this.options);
-		}
-	  
-		loadImages() {
-		  const images = $('img');
-	  
-		  images.each((index, img) => {
-			this.observer.observe(img);
-		  });
-	  
-		  const blurDivs = $(".blur-load");
-	  
-		  blurDivs.each(function () {
-			const img = $(this).find("img");
-	  
-			const loaded = () => {
-			  $(this).addClass('loaded');
-			};
-	  
-			if (img[0].complete) {
-			  loaded();
-			} else {
-			  img.on('load', loaded);
-			}
-		  });
-	  
-		  images.click(function () {
-			const src = $(this).attr('src');
-			const modal = $(`<div class="modal">
-			  <div class="modal-dialog">
-				<div class="modal-content">
-				  <img class="modal-img" src="${src}">
-				</div>
-			  </div>
-			</div>`);
-	  
-			modal.appendTo('body');
-	  
-			modal.show();
-			$('body').css('overflow', 'hidden');
-	  
-			modal.click(function () {
-			  modal.hide();
-			  modal.remove();
-			  $('body').css('overflow', 'visible');
-			});
-		  });
-		}
-	  
-		handleIntersection(entries, observer) {
-		  entries.forEach(entry => {
-			if (entry.isIntersecting) {
-			  const imgElement = entry.target;
-			  this.lazyLoadImage(imgElement);
-			  observer.unobserve(imgElement);
-			}
-		  });
-		}
-	  
-		lazyLoadImage(imgElement) {
-		  const xhr = new XMLHttpRequest();
-		  const src = $(imgElement).attr('src');
-	  
-		  const promise = new Promise((resolve, reject) => {
-			xhr.open('GET', src, true);
-			xhr.responseType = 'blob';
-			xhr.onload = function () {
-			  if (this.status === 200) {
-				const blob = this.response;
-				$(imgElement).attr('src', URL.createObjectURL(blob));
-				resolve();
-			  } else {
-				reject(new Error('Failed to load image'));
-			  }
-			};
-			xhr.onerror = function () {
-			  reject(new Error('Failed to load image'));
-			};
-			xhr.send();
-		  });
-	  
-		  promise.then(() => {
-			const parentDiv = $(imgElement).parent();
-			parentDiv.addClass('loaded');
-		  }).catch(error => {
-			console.error(error);
-		  });
-		}
-	  }
-
-	const lazyImageLoader = new LazyImageLoader();
-  	lazyImageLoader.loadImages(); 
-	  */
 
 	  class LazyImageLoader {
 		constructor() {
