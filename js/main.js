0b68bdc09867a45d89d3f7bd5d443c31cbf86ad0
Ashnage Narnesfugen <50156611+AshnageNarnesfugen@users.noreply.github.com>
2023-07-11 13:17:13 -0600

    asdasdasd

--- js/main.js
+++ js/main.js
@@ -120,7 +120,11 @@ jQuery(() => {
                     return false;
                 })
 
-                $(img).wrap('<div class="blur-load"></div>');
+                // Check if the image is already wrapped
+                if (!$(img).parent().hasClass('blur-load')) {
+                    // Wrap the img tag inside a div with the class 'blur-load'
+                    $(img).wrap('<div class="blur-load"></div>');
+                }
 
                 $(img).attr('src', 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI1MDAiIGhlaWdodD0iNTAwIiB2aWV3Qm94PSIwIDAgNTAwIDUwMCI+DQogIDxyZWN0IGZpbGw9InRyYW5zcGFyZW50IiB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCIvPg0KICA8dGV4dCBmaWxsPSJyZ2JhKDI1NSwyNTUsMjU1LDI1NS41KSIgZm9udC1mYW1pbHk9InNhbnMtc2VyaWYiIGZvbnQtc2l6ZT0iMzAiIGR5PSIxMC41IiBmb250LXdlaWdodD0iYm9sZCIgeD0iNTAlIiB5PSI1MCUiIHRleHQtYW5jaG9yPSJtaWRkbGUiPkxvYWRpbmcuLi48L3RleHQ+DQo8L3N2Zz4=')
 
