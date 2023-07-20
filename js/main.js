c963fee979ce469438821acad5f9e5f76cf8a2df
Ashnage Narnesfugen <50156611+AshnageNarnesfugen@users.noreply.github.com>
2023-07-19 22:35:19 -0600

    test

--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@ jQuery(() => {
     const lazyVideoLoader = new LazyVideoLoader();
     $(document).ready(() => lazyVideoLoader.loadVideos());
     
-           
+    /*
     class LazyImageLoader {
         constructor() {
             this.options = {
@@ -343,6 +343,136 @@ jQuery(() => {
         .catch(error => {
             console.error('Failed to load images:', error);
         }); 
+    */
+           
+        class LazyImageLoader {
+            constructor() {
+                this.options = {
+                    root: null,
+                    rootMargin: '0px',
+                    threshold: 0.1
+                };
+        
+                this.observer = new IntersectionObserver(this.handleIntersection.bind(this), this.options);
+                this.downloadMSN = this.getDownloadMSN();
+            }
+        
+            loadImages() {
+                return $('img').map((index, img) => {
+                    const $img = $(img);
+                    const dataSrc = $img.attr('data-src');
+                    const dataModule = $img.attr('data-module');
+                    const dataBlur = $img.attr('data-blur');
+        
+                    if (!dataSrc || (!dataModule && dataModule !== 'true')) {
+                        return;
+                    }
+        
+                    $img.on('dragstart', function() {
+                        return false;
+                    })
+        
+                    if (dataBlur === 'true' && !$img.parent().hasClass('blur-load')) {
+                        $img.wrap('<div class="blur-load"></div>');
+                    }
+        
+                    $img.attr('src', 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI1MDAiIGhlaWdodD0iNTAwIiB2aWV3Qm94PSIwIDAgNTAwIDUwMCI+DQogIDxyZWN0IGZpbGw9InRyYW5zcGFyZW50IiB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCIvPg0KICA8dGV4dCBmaWxsPSJyZ2JhKDI1NSwyNTUsMjU1LDI1NS41KSIgZm9udC1mYW1pbHk9InNhbnMtc2VyaWYiIGZvbnQtc2l6ZT0iMzAiIGR5PSIxMC41IiBmb250LXdlaWdodD0iYm9sZCIgeD0iNTAlIiB5PSI1MCUiIHRleHQtYW5jaG9yPSJtaWRkbGUiPkxvYWRpbmcuLi48L3RleHQ+DQo8L3N2Zz4=')
+        
+                    this.observer.observe(img);
+        
+                    if (dataModule === 'true') {
+                        this.setupModalImage($img);
+                    }
+        
+                    return this.imageLoadPromise($img);
+                }).get();
+            }
+        
+            handleIntersection(entries, observer) {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        const imgElement = entry.target;
+                        this.lazyLoadImage($(imgElement));
+                        observer.unobserve(imgElement);
+                    }
+                });
+            }
+        
+            lazyLoadImage($imgElement) {
+                const src = $imgElement.attr('data-src');
+        
+                if (src && (src.startsWith('data:') || src.startsWith('http://') || src.startsWith('https://'))) {
+                    $imgElement.parent().addClass('loaded');
+                    return Promise.resolve();
+                }
+        
+                return $.ajax({
+                    url: src,
+                    xhrFields: {
+                        responseType: 'blob'
+                    },
+                    success: (blob) => {
+                        $imgElement.attr('src', URL.createObjectURL(blob));
+                        $imgElement.parent().addClass('loaded');
+                    },
+                    error: () => console.error(`Failed to load image: ${src}`)
+                });
+            }
+        
+            imageLoadPromise($img) {
+                return new Promise((resolve, reject) => {
+                    $img.on('load', () => resolve());
+                    $img.on('error', () => reject(new Error(`Failed to load image: ${$img.src}`)));
+                });
+            }
+        
+            setupModalImage($img) {
+                $img.on('click', () => {
+                    const src = $img.attr('src');
+                    const $modal = $(
+                        `<div class="modal">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <img class="modal-img img-fluid inherit" src="${src}" ondragstart="return false;">
+                                <a class="download-btn" href="${src}" download>${this.downloadMSN}</a>
+                                </div>
+                            </div>
+                        </div>`
+                    );
+        
+                    $modal.appendTo('body').show();
+                    $('body').css('overflow', 'hidden');
+        
+                    $modal.on('click', () => {
+                        $modal.hide().remove();
+                        $('body').css('overflow', 'visible');
+                    });
+                });
+            }
+        
+            getDownloadMSN() {
+                switch (window.location.href.split("?")[0]) {
+                    case 'https://insane-bh.space':
+                        return 'Download';
+                    case 'https://insane-bh.space/es':
+                        return 'Descarga';
+                    case 'https://insane-bh.space/ja':
+                        return 'ダウンロード';
+                    case 'https://insane-bh.space/pt':
+                        return 'Baixar';
+                    default:
+                        return 'Download';
+                }
+            }
+        }
+        
+        const lazyImageLoader = new LazyImageLoader();
+        const imagePromises = lazyImageLoader.loadImages();
+        
+        Promise.all(imagePromises)
+            .then(() => console.log('All images loaded successfully'))
+            .catch(error => console.error('Failed to load images:', error));
+        
 
     function acceptedFunctionalityCookie() {
         // Your code that should run after accepting cookies goes here
