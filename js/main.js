f4822895ef809a7146677834f2f6357f777a90e1
Ashnage Narnesfugen <50156611+AshnageNarnesfugen@users.noreply.github.com>
2023-07-23 13:57:53 -0600

    dsdfsdfsdf

--- js/main.js
+++ js/main.js
@@ -572,7 +572,7 @@ jQuery(() => {
                 sameSite: 'Strict',
             });
         
-            let url = `${path}?country=${country}`;
+            let url = `${this.baseUrl}${path}?country=${country}`;
         
             if (data) {
                 url += `&region=${data.region}&city=${data.city}&currency=${data.currency}`;
@@ -582,8 +582,7 @@ jQuery(() => {
             }
         
             window.location.href = url;
-        }
-                   
+        }           
     }    
 
     class CookieConsentHandler {
