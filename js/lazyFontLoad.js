7b480199f9c60aa5d4884bd616a219ee6394f27e
Ashnage Narnesfugen <50156611+AshnageNarnesfugen@users.noreply.github.com>
2023-08-11 21:22:19 -0600

    Test

--- js/lazyFontLoad.js
+++ js/lazyFontLoad.js
@@ -1,4 +1,5 @@
 jQuery(() => {
+    /*
     (function($) {
         $.fn.fontLoader = function(options) {
             const settings = $.extend({
@@ -54,6 +55,72 @@ jQuery(() => {
             }).catch(error => {
                 console.error('Some fonts failed to load.', error);
                 $(this).trigger('fontsLoadError', error);
+            });
+        };
+    })(jQuery);
+    */
+    (function($) {
+        $.fn.fontLoader = function(options) {
+            const settings = $.extend({
+                fonts: [],
+                fallback: 'sans-serif',
+                timeout: 3000
+            }, options);
+    
+            function isFontAvailable(fontName) {
+                // This checks if the font might be available locally
+                const font = new FontFaceObserver(fontName);
+                return font.check();
+            }
+
+            function loadFontWithTimeout(font) {
+                return new Promise((resolve, reject) => {
+                    const timer = setTimeout(() => {
+                        reject(new Error(`Font loading timeout for ${font.name}`));
+                    }, settings.timeout);
+
+                    loadFont(font).then(loadedFont => {
+                        clearTimeout(timer);
+                        resolve(loadedFont);
+                    }, reject);
+                });
+            }
+
+            function loadFont(font) {
+                if (isFontAvailable(font.name)) {
+                    return Promise.resolve();
+                }
+
+                const formats = font.formats || {};
+                const promises = [];
+    
+                for (let format in formats) {
+                    const face = new FontFace(font.name, `url(${formats[format]})`, {
+                        weight: font.weight || 'normal',
+                        style: font.style || 'normal'
+                    });
+                    promises.push(face.load());
+                }
+    
+                return Promise.all(promises).then(loadedFonts => {
+                    loadedFonts.forEach(loadedFont => {
+                        document.fonts.add(loadedFont);
+                    });
+                }).catch(error => {
+                    console.error(`Failed to load font: ${font.name}`, error);
+                });
+            }
+    
+            const fontPromises = settings.fonts.map(loadFontWithTimeout);
+    
+            return this.each(function() {
+                Promise.all(fontPromises).then(() => {
+                    console.log('All fonts loaded!');
+                    $(this).trigger('fontsLoaded');
+                }).catch(error => {
+                    console.error('Some fonts failed to load.', error);
+                    $(this).trigger('fontsLoadError', error);
+                });
             });
         };
     })(jQuery);
