4978279cc5e951478c8777a91c2f8b4df5644e8f
Ashnage Narnesfugen <50156611+AshnageNarnesfugen@users.noreply.github.com>
2023-08-15 14:39:13 -0600

    Test

--- js/lazyBackgroundImgLoader.js
+++ js/lazyBackgroundImgLoader.js
@@ -1,3 +1,4 @@
+/*
 (function($) {
     $.fn.lazyBackgroundLoader = function(options) {
         const settings = $.extend({
@@ -65,6 +66,83 @@
 
         return this.each(function() {
             const backgroundPromises = loadBackgrounds.call($(this));
+
+            Promise.all(backgroundPromises)
+                .then(() => console.log('All backgrounds loaded successfully'))
+                .catch(error => console.error('Failed to load backgrounds:', error));
+        });
+    };
+}(jQuery));
+*/
+(function($) {
+    $.fn.lazyBackgroundLoader = function(options) {
+        const defaultSettings = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.1,
+        };
+
+        const settings = $.extend(defaultSettings, options);
+        const observer = new IntersectionObserver(handleIntersection, settings);
+
+        function isValidURL(src) {
+            return /^(data:|https?:\/\/)/.test(src);
+        }
+
+        function lazyLoadBackground($divElement) {
+            const src = $divElement.attr('data-background-img');
+
+            if (!src || !isValidURL(src)) {
+                return Promise.resolve();
+            }
+
+            if (src.startsWith('data:')) {
+                $divElement.addClass('loaded');
+                return Promise.resolve();
+            }
+
+            return $.ajax({
+                url: src,
+                xhrFields: {
+                    responseType: 'blob'
+                }
+            })
+            .then((blob) => {
+                $divElement.css('background-image', `url(${URL.createObjectURL(blob)})`);
+                $divElement.addClass('loaded');
+            })
+            .catch(() => console.error(`Failed to load background image: ${src}`));
+        }
+
+        function handleIntersection(entries) {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const divElement = entry.target;
+                    lazyLoadBackground($(divElement));
+                    observer.unobserve(divElement);
+                }
+            });
+        }
+
+        function backgroundLoadPromise($div) {
+            return new Promise((resolve, reject) => {
+                const img = new Image();
+                img.onload = resolve;
+                img.onerror = () => reject(new Error(`Failed to load background image: ${$div.attr('data-background-img')}`));
+                img.src = $div.attr('data-background-img');
+            });
+        }
+
+        return this.each(function() {
+            const backgroundPromises = $(this).map((index, divElement) => {
+                const $divElement = $(divElement);
+                const dataBackgroundImg = $divElement.attr('data-background-img');
+
+                if (dataBackgroundImg) {
+                    observer.observe(divElement);
+                    return backgroundLoadPromise($divElement);
+                }
+            }).get();
 
             Promise.all(backgroundPromises)
                 .then(() => console.log('All backgrounds loaded successfully'))
