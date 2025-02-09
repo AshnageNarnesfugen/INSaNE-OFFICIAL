c00808da60c24dcb3a2fd649da9be29a003e4a1b
Dread Ashes <50156611+AshnageNarnesfugen@users.noreply.github.com>
2025-02-09 04:29:30 -0600

    done

--- js/lazyFontLoad.js
+++ js/lazyFontLoad.js
@@ -6,59 +6,71 @@ jQuery(() => {
         } = options;
 
         function loadFont(font) {
-            const { name, formats = {}, weight = 'normal', style = 'normal' } = font;
-            const promises = Object.entries(formats).map(([format, url]) => {
-                const face = new FontFace(name, `url(${url})`, { weight, style });
-                return face.load().then(loadedFont => {
-                    document.fonts.add(loadedFont);
-                }).catch(error => {
-                    console.error(`Failed to load font [${name}] in format [${format}]:`, error);
+            const { name, formats = {}, weight = 'normal', style = 'normal', priority = 'lazy' } = font;
+            try {
+                if (priority === 'preload' || priority === 'prefetch') {
+                    Object.entries(formats).forEach(([format, url]) => {
+                        const link = document.createElement('link');
+                        link.rel = priority;
+                        link.href = url;
+                        link.as = 'font';
+                        link.type = `font/${format}`;
+                        link.crossOrigin = 'anonymous';
+                        document.head.appendChild(link);
+                    });
+                }
+                
+                Object.entries(formats).forEach(([format, url]) => {
+                    const face = new FontFace(name, `url(${url})`, { weight, style });
+                    face.load().then(loadedFont => document.fonts.add(loadedFont)).catch(error => {
+                        console.error(`Failed to load font [${name}] in format [${format}]:`, error);
+                    });
                 });
-            });
-            return Promise.all(promises);
+                console.log(`Font ${name} processed.`);
+            } catch (error) {
+                console.error(`Error processing font [${name}]:`, error);
+            }
         }
 
-        const fontPromises = fonts.map(loadFont);
-
-        return Promise.all(fontPromises).then(() => {
-            console.log('All fonts loaded!');
-            this.trigger('fontsLoaded');
-        }).catch(error => {
-            console.error('Some fonts failed to load.', error);
-            this.trigger('fontsLoadError', error);
-        });
+        fonts.forEach(loadFont);
+        console.log('Font loading initiated.');
+        $(document).trigger('fontsLoaded');
     };
 
     $(window).on('load', function () {
         const currentURL = window.location.href.toLowerCase();
         let fontsToLoad = [];
 
-        if (currentURL.includes('/ja')) {
-            fontsToLoad = [
+        const fontConfigs = {
+            '/ja': [
                 {
                     name: 'Noto Serif Hentaigana',
+                    weight: '400',
+                    style: 'normal',
+                    priority: 'preload',
                     formats: {
                         ttf: '../css/fonts/NotoSerifHentaigana-VariableFont_wght.ttf'
                     }
                 }
-            ];
-        } else if (currentURL.includes('/ru')) {
-            fontsToLoad = [
+            ],
+            '/ru': [
                 {
                     name: 'MailSans',
+                    weight: '400',
+                    style: 'normal',
+                    priority: 'preload',
                     formats: {
                         woff2: '../css/fonts/MailSansRegular.woff2',
                         woff: '../css/fonts/MailSansRegular.woff'
                     }
                 }
-            ];
-        } else {
-            // Default fonts for other pages
-            fontsToLoad = [
+            ],
+            'default': [
                 {
                     name: 'sharpsans-web',
                     weight: '400',
                     style: 'normal',
+                    priority: 'lazy',
                     formats: {
                         woff2: '../css/fonts/SharpSans-Medium.woff2'
                     }
@@ -67,6 +79,7 @@ jQuery(() => {
                     name: 'sharpsans-web',
                     weight: '600',
                     style: 'normal',
+                    priority: 'lazy',
                     formats: {
                         woff2: '../css/fonts/SharpSans-Semibold.woff2'
                     }
@@ -75,18 +88,22 @@ jQuery(() => {
                     name: 'sharpsans-web',
                     weight: '800',
                     style: 'normal',
+                    priority: 'lazy',
                     formats: {
                         woff2: '../css/fonts/SharpSans-Bold.woff2'
                     }
                 },
                 {
                     name: 'architects-daughter',
+                    priority: 'preload',
                     formats: {
                         ttf: '../css/fonts/ArchitectsDaughter-Regular.ttf'
                     }
                 }
-            ];
-        }
+            ]
+        };
+
+        fontsToLoad = fontConfigs[Object.keys(fontConfigs).find(key => currentURL.includes(key))] || fontConfigs['default'];
 
         $('body').fontLoader({
             fonts: fontsToLoad,
