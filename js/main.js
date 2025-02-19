536d79fecd13f4691d0a622fbb1a32455e71c717
Dread Ashes <50156611+AshnageNarnesfugen@users.noreply.github.com>
2025-02-19 14:38:48 -0600

    asdasdasd

--- js/main.js
+++ js/main.js
@@ -297,7 +297,7 @@ jQuery(() => {
     var index = 0
     const interval = () => {
         container.shuffleLetters({
-            "step": 15, // adjusted from 30 to 15
+            "step": 30, // adjusted from 30 to 15
             "fps": 60,
             "text": data[index]
         });
