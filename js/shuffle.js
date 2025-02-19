44b4777072f786d30fdbfc0b0648cd5cf553cfab
Dread Ashes <50156611+AshnageNarnesfugen@users.noreply.github.com>
2025-02-19 13:11:12 -0600

    test

--- js/shuffle.js
+++ js/shuffle.js
@@ -1,4 +1,4 @@
-$.fn.shuffleLetters = function(prop) {
+/*$.fn.shuffleLetters = function(prop) {
     var options = $.extend({
         "step": 20, // How many times should the letters be changed
         "fps": 30, // Frames Per Second
@@ -111,4 +111,127 @@ function randomChar(type) {
         hindi: "अआइईउऊऋएऐऑओऔकखगघङचछजझञटठडढणतथदधनपफबभमयरलवशषसह़ािीुूृेैॉोौ्ॐऽ।॥"
     };
     return pools[type] ? pools[type][Math.floor(Math.random() * pools[type].length)] : "";
-}
+}*/
+
+$.fn.shuffleLetters = function(prop) {
+    var options = $.extend({
+        "step": 20, // How many times should the letters be changed
+        "fps": 30, // Frames Per Second
+        "text": null // Use this text instead of the contents
+    }, prop);
+
+    return this.each(function() {
+        var el = $(this);
+        var textNodes = [];
+
+        function extractTextNodes(node) {
+            if (node.nodeType === Node.TEXT_NODE) {
+                textNodes.push(node);
+            } else {
+                $(node).contents().each(function() {
+                    extractTextNodes(this);
+                });
+            }
+        }
+
+        if (options.text !== null) {
+            textNodes = [{ nodeValue: options.text }];
+        } else {
+            extractTextNodes(el[0]);
+        }
+
+        var letters = [];
+        var types = [];
+
+        textNodes.forEach((node, index) => {
+            var str = node.nodeValue.split('');
+            letters[index] = [];
+            types[index] = [];
+
+            for (var i = 0; i < str.length; i++) {
+                var ch = str[i];
+                switch (true) {
+                    case ch === " ":
+                        types[index][i] = "space";
+                        break;
+                    case /[a-z]/.test(ch):
+                        types[index][i] = "lowerLetter";
+                        break;
+                    case /[A-Z]/.test(ch):
+                        types[index][i] = "upperLetter";
+                        break;
+                    case /[0-9]/.test(ch):
+                        types[index][i] = "number";
+                        break;
+                    case /[\u3040-\u309F]/.test(ch):
+                        types[index][i] = "hiragana";
+                        break;
+                    case /[\u30A0-\u30FF]/.test(ch):
+                        types[index][i] = "katakana";
+                        break;
+                    case /[\u4E00-\u9FBF]/.test(ch):
+                        types[index][i] = "kanji";
+                        break;
+                    case /[\u4E00-\u9FFF]/.test(ch):
+                        types[index][i] = "chinese";
+                        break;
+                    case /[\uAC00-\uD7A3]/.test(ch):
+                        types[index][i] = "korean";
+                        break;
+                    case /[\u0410-\u044F]/.test(ch):
+                        types[index][i] = "russian";
+                        break;
+                    case /[\u0600-\u06FF]/.test(ch):
+                        types[index][i] = "arabic";
+                        break;
+                    case /[\u0900-\u097F]/.test(ch):
+                        types[index][i] = "hindi";
+                        break;
+                    default:
+                        types[index][i] = "symbol";
+                }
+                letters[index].push(i);
+            }
+        });
+
+        function shuffle(start) {
+            if (start > options.step) return;
+
+            textNodes.forEach((node, index) => {
+                var strCopy = node.nodeValue.split('');
+                letters[index].forEach((pos, i) => {
+                    if (i < start + options.step) {
+                        strCopy[pos] = $.fn.shuffleLetters.randomChar(types[index][pos]);
+                    }
+                });
+                if (options.text !== null) {
+                    el.text(strCopy.join(""));
+                } else {
+                    node.nodeValue = strCopy.join("");
+                }
+            });
+
+            setTimeout(() => shuffle(start + 1), 1000 / options.fps);
+        }
+
+        shuffle(-options.step);
+    });
+};
+
+$.fn.shuffleLetters.randomChar = function(type) {
+    const pools = {
+        lowerLetter: "abcdefghijklmnopqrstuvwxyz0123456789",
+        upperLetter: "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        number: "0123456789",
+        symbol: ",.?/\\(^)![]{}*&^%$#'\"",
+        hiragana: "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん",
+        katakana: "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン",
+        kanji: "亜哀挨愛曖悪握圧扱宛嵐安案暗以衣位囲医依委威為畏胃尉異移萎偉椅彙意違維慰遺緯射捨赦謝詐社車舎者尺借酌釈爵若樹受呪寿授需儒舟酒収宗就縦従縮熟純処初所暑署書諸除傷償勝称笑賞上",
+        chinese: "的一是在不了有和人这中大为上个国我以要他时来用们生到作地于出就分对成会可主发年动同工也能下过子说产种面而方后多定行学法所民得经",
+        korean: "가각간갇갈감갑값갓갔강갖같갚갛개객갠갤갬갭갯갰갱갸갹갼걀걋걍걔걘걜",
+        russian: "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя",
+        arabic: "ءآأؤإئابةتثجحخدذرزسشصضطظعغفقكلمنهوىيـًٌٍَُِّْ٠١٢٣٤٥٦٧٨٩",
+        hindi: "अआइईउऊऋएऐऑओऔकखगघङचछजझञटठडढणतथदधनपफबभमयरलवशषसह़ािीुूृेैॉोौ्ॐऽ।॥"
+    };
+    return pools[type] ? pools[type][Math.floor(Math.random() * pools[type].length)] : "";
+};
