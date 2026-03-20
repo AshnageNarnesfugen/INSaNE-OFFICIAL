9cccafafed385faaa49d95fd50f4ffa5f3dadf51
Dread Ashes <50156611+AshnageNarnesfugen@users.noreply.github.com>
2026-03-20 19:37:49 +0000

    asdfasfafs

--- js/lazyImageLoader.js
+++ js/lazyImageLoader.js
@@ -73,7 +73,6 @@
                 }
 
                 const $container = $img.parent();
-                $container.addClass('cursor-container')
                 const $cursor = $(`<div class="custom-cursor-pill">${ settings.cursorText }</div>`).appendTo($container);
 
                 $img.attr('src', 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI1MDAiIGhlaWdodD0iNTAwIiB2aWV3Qm94PSIwIDAgNTAwIDUwMCI+DQogIDxyZWN0IGZpbGw9InRyYW5zcGFyZW50IiB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCIvPg0KICA8dGV4dCBmaWxsPSJyZ2JhKDI1NSwyNTUsMjU1LDI1NS41KSIgZm9udC1mYW1pbHk9InNhbnMtc2VyaWYiIGZvbnQtc2l6ZT0iMzAiIGR5PSIxMC41IiBmb250LXdlaWdodD0iYm9sZCIgeD0iNTAlIiB5PSI1MCUiIHRleHQtYW5jaG9yPSJtaWRkbGUiPkxvYWRpbmcuLi48L3RleHQ+DQo8L3N2Zz4=')
@@ -83,6 +82,7 @@
                 if (dataModule === 'true') {
                     setupModalImage($img);
                     // --- Lógica de GSAP para el seguimiento ---
+                    $container.addClass('cursor-container')
                     $container.on('mousemove', (e) => {
                         const rect = $container[0].getBoundingClientRect();
                         const x = e.clientX - rect.left;
