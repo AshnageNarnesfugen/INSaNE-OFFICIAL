b0dd953fba817a7cf689753f6511120785f55736
Dread Ashes <50156611+AshnageNarnesfugen@users.noreply.github.com>
2026-04-01 12:22:21 -0600

    yes

--- js/LazyVideoLoad.js
+++ js/LazyVideoLoad.js
@@ -8,8 +8,8 @@
             endWidth: "100%",
             startRadius: "40px",
             endRadius: "0px",
-            gsapStart: "top 40%",
-            gsapEnd: "+=800"
+            gsapStart: "top 50%",
+            gsapEnd: "top 10%"
         }, options);
 
         if (typeof gsap !== "undefined" && typeof ScrollTrigger !== "undefined") {
